ede35a7ff1e0490f7104a240cd895986e5e6a01a
ansimytech <53067495+ansimytech@users.noreply.github.com>
2019-08-01 21:35:41 +1000

    Update index.js
    Suitable changes done to include callback and display all regions and zones.

--- index.js
+++ index.js
@@ -7,24 +7,40 @@ var ec2 = new AWS.EC2({apiVersion: '2016-11-15',region: 'eu-west-2'});
 
 var params = {};
 
-exports.handler = function(event, context, callback) {
 
-// Retrieves all regions/endpoints that work with EC2
-ec2.describeRegions(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Regions: ", data.Regions);
-  }
-});
+var awsCallback = function(data) {
 
-// Retrieves availability zones only for region of the ec2 service object
-ec2.describeAvailabilityZones(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Availability Zones: ", data.AvailabilityZones);
-  }
-});
+  ec2.describeRegions(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    }
+  
+  
+    var regionArray = data.Regions;
+  
+    regionArray.forEach(element => {
+        ec2 = new AWS.EC2({apiVersion: '2016-11-15',region: element.RegionName});
+  
+        ec2.describeAvailabilityZones(params, function(err, data) {
+            if (err) {
+              console.log("Error", err);
+            } else {
+                  data.AvailabilityZones.forEach(nestedElement => {
+                      console.log("{ \"region_zones\"", ":\"" + nestedElement.RegionName, nestedElement.ZoneName  + "\"}");
+                  }); 
+  
+            }
+  
+        });
+  
+    });
+  
+  });
 
-};
+  };
+  
+  var describeAWSRegions = function(callback) {
+    callback('All Regions and Available Zones Displayed');
+  };
+
+  describeAWSRegions(awsCallback);
